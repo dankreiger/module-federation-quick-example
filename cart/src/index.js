e34474370d8cca738dd2061bc82df18eb5ecee42
Dan Kreiger <dan@dankreiger.com>
2021-02-10 09:15:23 +0100

    chore(deps): add shared dep config

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,7 +1,10 @@
+import faker from 'faker';
+
 document.querySelector('#dev-cart').innerHTML = `
   <div style="border: 1px dotted blue; color: blue; padding: 10px;">
     <h1>Microfrontend 2</h1>
     <div>Shopping Cart</div>
+    <div>Random # of items: ${faker.random.number()}</div>
   </div>
   
 `;
--- products/src/index.js
+++ products/src/index.js
@@ -1,16 +1,1 @@
-import faker from 'faker';
-
-let products = '';
-
-const injectHtml = (html) =>
-  `<div style="border: 1px dotted red; color: red; padding: 10px;">
-    <h1>Microfrontend 1</h1>
-    ${html}
-  </div>`;
-
-for (let i = 0; i < 5; i++) {
-  const name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
-}
-
-document.querySelector('#dev-products').innerHTML = injectHtml(products);
+import('./boostrap');
