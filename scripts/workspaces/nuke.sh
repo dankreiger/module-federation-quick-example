88bc39c7bcb4e506d7be16012d2f8fd9b13f3b77
Dan Kreiger <dan@dankreiger.com>
2021-02-20 00:19:38 +0100

    chore(cart,container,products): refresh deps

--- scripts/workspaces/nuke.sh
+++ scripts/workspaces/nuke.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo "┏━━━ 💣 NUKE ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-lerna run --scope @microwelpe/* --parallel nuke
+lerna run --scope @microwelpe/* --parallel nuke && rimraf node_modules
